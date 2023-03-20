6ab0aaa5ac68fc7f55f1ec8f4a0dc5f62100896a
Katacooper <francescoiliceto0@gmail.com>
2023-03-20 12:01:47 +0100

    Main.java

---
+++ Main.java
@@ -0,0 +1,95 @@
+package javabasics._16;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+    }
+
+
+    /**
+     * 1: Take the code below and put it into a function, read the code and see what it is.
+     * <p>
+     * What do you think the function does? Name the function appropriately and make sure
+     * <p>
+     * return the appropriate result type
+     *
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1:");
+        // Put this into a function
+        double celsius = 30;
+        double fahrenheit = 0;
+        celsiusFahrenheit(30,0);
+        // End
+    }
+
+
+
+    /**
+     * 2: Create a function that processes and prints out all the names in the ArrayList
+     *
+     *    Make sure to put all the formatting code in the function, formatting:
+     *    1) to Upper case
+     *    2) Remove spaces at the start and the end
+     *    3) If the name contains any numbers, instead of returning the name, return "Invalid"
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        List<String> names = Arrays.asList("Alan   ", "Alice   ", "Ant0ni0   ", "  Aziz");
+
+        for (String name : names) {
+            // Call your function
+            conversionList(name);
+        }
+    }
+
+    /**
+     * 3: Write a function that prints out every variable seen in the code. It should not return anything
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+
+        double myLunchPrice = 5.99;
+        String lunchType = "sandwich";
+        String description = "Nice big sandwich";
+        int weightInGrams = 500;
+
+        // Call your function
+        Callings(myLunchPrice,lunchType,description,weightInGrams);
+
+    }
+
+    private static void celsiusFahrenheit(double celsius, double fahrenheit)
+    {
+        fahrenheit = ((celsius * 9) / 5) + 32;
+        String conversionDescription = celsius + " Celsius is " + fahrenheit + " fahrenheit";
+        System.out.println(conversionDescription);
+    }
+
+    private static String conversionList(String name)
+    {
+        boolean containDigit = false;
+        name = name.toUpperCase().trim();
+        for (char c : name.toCharArray())
+        {
+            if(containDigit = Character.isDigit(c))
+            {
+                System.out.println("Invalid");
+                return name;
+            }
+        }
+        System.out.println(name);
+        return name;
+    }
+    private static void Callings(double first, String second, String third, int fourth)
+    {
+        System.out.println(first + "\n" + second + "\n" + third + "\n" + fourth);
+    }
+}
+
+
